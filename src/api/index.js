93f834cb0fce5d9d0aeafd54eaa82591947307ef
marven mathelier <marven250@hotmail.com>
2021-09-30 11:38:04 -0400

    https

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useSelector } from 'react-redux';
 
 
-const url = 'http://blooming-spire-52990.herokuapp.com/volcanoes'
+const url = 'https://blooming-spire-52990.herokuapp.com/volcanoes'
 
 
 export const fetchVolcanoes = () => axios.get(url);
